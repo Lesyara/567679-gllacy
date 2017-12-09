dc2ab061803f0040442592d34749de9b20e11cb2
Lesyara <lesya.tsyrulnikova@gmail.com>
2017-12-09 15:03:47 +0300

    исправление ошибок по критериям № 2
    отступы по автоформату и замена фона на линейный градиент

--- js/script.js
+++ js/script.js
@@ -6,22 +6,22 @@ var email_field = document.querySelector(".feedback_usermail input");
 var text_field = document.querySelector(".feedback_usertext textarea");
 var dark_wrap = document.querySelector(".darkening_wrapper");
 
-if(localStorage) {
+if (localStorage) {
   var saved_login = localStorage.getItem("userLogin");
-  var saved_mail = localStorage.getItem("userMail");    
+  var saved_mail = localStorage.getItem("userMail");
 }
 
-open_button.addEventListener("click", function(evt) {
+open_button.addEventListener("click", function (evt) {
   evt.preventDefault();
-  feedback_form.classList.add("feedback_show"); 
+  feedback_form.classList.add("feedback_show");
   dark_wrap.classList.add("darkening_on");
-  
-  if(saved_login && saved_mail) {
+
+  if (saved_login && saved_mail) {
     username_field.value = saved_login;
-    email_field.value = saved_mail; 
+    email_field.value = saved_mail;
     text_field.focus();
   }
-  else if(saved_login) {
+  else if (saved_login) {
     username_field.value = saved_login;
     email_field.focus();
   }
@@ -30,15 +30,15 @@ open_button.addEventListener("click", function(evt) {
   }
 });
 
-close_button.addEventListener("click", function(evt) {
+close_button.addEventListener("click", function (evt) {
   evt.preventDefault();
   feedback_form.classList.remove("feedback_show");
   feedback_form.classList.remove("feedback_submit_error");
   dark_wrap.classList.remove("darkening_on");
 });
 
-feedback_form.addEventListener("submit", function(evt) {
-  if( !username_field.value || !email_field.value ) {
+feedback_form.addEventListener("submit", function (evt) {
+  if (!username_field.value || !email_field.value) {
     evt.preventDefault();
     feedback_form.classList.remove("feedback_submit_error");
     feedback_form.offsetWidth = feedback_form.offsetWidth;
@@ -54,10 +54,10 @@ feedback_form.addEventListener("submit", function(evt) {
     localStorage.setItem("userLogin", username_field.value);
     localStorage.setItem("userMail", email_field.value);
   }
-}); 
+});
 
-window.addEventListener("keydown",function(evt) {
-  if(evt.keyCode == 27) {
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode == 27) {
     if (feedback_form.classList.contains("feedback_show") && dark_wrap.classList.contains("darkening_on")) {
       feedback_form.classList.remove("feedback_show");
       feedback_form.classList.remove("feedback_submit_error");
